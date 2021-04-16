aad606e4fe76a5c845815eb599d18019de6b1eb1
AGgethub <ags_313@hotmail.com>
2021-04-15 18:06:27 -0700

    comit

--- ReactFirst/robofriends/src/Hello.js
+++ ReactFirst/robofriends/src/Hello.js
@@ -3,9 +3,9 @@ import './Hello.css';
 class Hello extends Component {
     render () {
         return (
-        <div> 
+        <div className='f1 tc'> 
             <h1>Hello World</h1>
-            <p>Welcome to React</p>
+            <p>{this.props.greeting}</p>
         </div>
         )
     }
