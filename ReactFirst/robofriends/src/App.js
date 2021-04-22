a23fe13cbfce8ce0cad9b91c7d87cd42f9e60fa5
AGgethub <ags_313@hotmail.com>
2021-04-21 19:06:49 -0700

    comit

--- ReactFirst/robofriends/src/App.js
+++ ReactFirst/robofriends/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots'
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -10,9 +10,17 @@ class App extends Component {
     constructor () {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+     
+    //this link with request the robot files from another site
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => this.setState({robots: users}));
+        
     }
 
     onSearchChange =(event) => {
@@ -24,17 +32,23 @@ class App extends Component {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
         
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
         return (
             <div className= 'tc' >   
                <h1 className='f1'>RoboFriends</h1>
                <SearchBox searchChange={this.onSearchChange}/>
-               <CardList robots={filteredRobots}/>
-   
+               <Scroll>
+                 <CardList robots={filteredRobots} />
+               </Scroll>
+               
             </div>
-      
-       );
-
+        );
+       
     }
+ 
+  }
 }
 
 export default App;
