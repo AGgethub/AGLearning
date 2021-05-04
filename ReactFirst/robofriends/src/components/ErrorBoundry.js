71440c1b48ababfe8164ea1a42500f7abf4c2b42
AGgethub <ags_313@hotmail.com>
2021-05-03 19:05:00 -0700

    comit

--- ReactFirst/robofriends/src/components/ErrorBoundry.js
+++ ReactFirst/robofriends/src/components/ErrorBoundry.js
@@ -7,19 +7,21 @@ class ErrorBoundry extends Component {
             hasError: false
         }
     }
- }
- //this code below   is a life cycle huck "componentDidCatch" Not sure how to fix but if i delete Error Boundry.js should go back to normal
 
- componentDidCatch(error, info) {
-     this.setState({hasError: true})
- }
 
- render () {
-     if (this.state.hasError) {
-         return <h1>Oooops. This is not good</h1>
-     }
-     return this.props.children
- }
+    componentDidCatch (error, info) {
+        this.setState({hasError: true})
 
- 
- export default ErrorBoundry;
+    }
+
+    render() {
+        if (this.state.hasError) {
+          return <h1>Oooops. That is not good</h1>
+        }
+        return this.props.children
+    }
+}
+
+
+
+export default ErrorBoundry;
